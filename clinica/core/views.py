24c1fc73a2c9e7dab4a54524fd8786c4f37847a0
Lucas Marchiori Pereira <Lucas@MacBook-Pro-de-Lucas.local>
2016-11-07 16:00:04 -0200

    adicionado template index

--- clinica/core/views.py
+++ clinica/core/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello Django')
+	lista_clientes = ['Marcos', 'lucas', 'felipe']
+	context = {
+		'title': 'Bem-vindo(a) a clínica!',
+		'clientes': lista_clientes
+	}
+	return render(request, 'index.html', context)
